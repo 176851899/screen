cff940994696f3da9000eae31c85971dda78cf1b
Seek <1911318541@qq.com>
2022-03-24 11:40:16 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,11 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 允许定义变量不使用
+    'no-unused-vars': 'off',
+    // 允许new
+    'no-new': 'off',
+    'no-undef': 'off',
   }
 }
