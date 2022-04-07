ec711ed07a08a47ab01162809f77544f5fb06f6e
Seek <1911318541@qq.com>
2022-04-07 22:52:31 +0800

    fix: 修复

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,17 +1,11 @@
 
 import Home from '../views/Home.vue'
-import dd from '../views/dd.vue'
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
-  },{
-    path: '/dd',
-    name: 'Home',
-    component: dd
   }
-
 ]
 
 export default routes
